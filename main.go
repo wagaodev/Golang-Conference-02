273770c6ff2f82adebe8d7f8eb24d73216e85e48
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-21 02:36:35 -0300

    feat: work with goroutines perfectly

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(workerId int, data chan int) {
+	for x := range data {
+		time.Sleep(time.Second)
+		fmt.Printf("Worker %d received %d\n", workerId, x)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World")
+	ch := make(chan int)
+	quantityWorkers := 10000000
+
+	for i := 0; i < quantityWorkers; i++ {
+		go worker(i, ch)
+	}
+
+	for i := 0; i <= 10000000; i++ {
+		ch <- i
+	}
 }
